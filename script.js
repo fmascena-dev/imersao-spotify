069d01d3f95981664dc80f782eeac4c4d3ac16fe
felipe mascena <felipe.mascena.dev@gmail.com>
2025-01-30 21:26:31 -0300

    inserindo buttons, search, saudação, cards

---
+++ script.js
@@ -0,0 +1,65 @@
+//BOM DIA | BOA TARDE | BOA NOITE
+
+// Obtém a referência do elemento com o ID "greeting"
+const greetingElement = document.getElementById("greeting");
+
+// Obtém a hora atual do sistema
+const currentHour = new Date().getHours();
+
+// Define a saudação com base na hora atual
+// if (currentHour >= 5 && currentHour < 12) {
+//   greetingElement.textContent = "Bom dia";
+// } else if (currentHour >= 12 && currentHour < 18) {
+//   greetingElement.textContent = "Boa tarde";
+// } else {
+//   greetingElement.textContent = "Boa noite";
+// }
+
+// Forma mais simples
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
+
+
+
+// ---------------------------------
+
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
